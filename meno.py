9781b6691492d368f7c6f7048c5bdea53086b843
NOMASKERFAN <166549249+NOMASKERFAN@users.noreply.github.com>
2024-09-03 23:55:58 +0330

    Add files via upload

---
+++ meno.py
@@ -0,0 +1,17 @@
+from colorama import Fore as c
+yeelow,green,blue,j,red=c.YELLOW,c.GREEN,c.BLUE,c.MAGENTA,c.RED
+panel=f"""
+{green}---------------------------                 
+|{yeelow}-->{green}[1]:{yeelow}run page          |
+|{yeelow}-->{green}[2]:{blue}download php      |
+|{yeelow}-->{green}[3]:{j}download ngrok    |
+|{yeelow}-->{green}[4]:{red}exit app          |
+{yeelow}|-------------------------|
+"""
+
+
+
+
+
+
+print(panel)
